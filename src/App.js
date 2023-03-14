ccfde5f09ca83931fb9c65ba8812f77b18eb5e22
Eunille <eunille.villegas@growsari.com>
2023-03-14 20:53:59 +0800

    remove app .tsx

---
+++ src/App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text>THIS IS TEST</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
